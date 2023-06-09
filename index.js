f2c90fe90789d1d4a490731b5986438039dfe5d5
Ian Hrydziuszko <132368002+robotsWhoSmokeCrack@users.noreply.github.com>
2023-06-08 21:12:54 -0400

    Add functionality to game in console and prompts

--- index.js
+++ index.js
@@ -40,21 +40,49 @@ const gameBoard = (() => {console.log("gameBoard ran")
         }
     })()
 
-// const Player = (name, marker) => {
-//     const joinGame = (() => console.log(`${name} has entered the game as ${marker}`))();
-    
-//     const move = (a,b) => {
-//         gameBoard.playerMove(a,b,marker);
-//         
-// }
-//   return { 
-//     name, 
-//     marker, 
-//     joinGame,
-//     move,
-//     };
 
-// };
+// const checkWin = (gameBoard.board.forEach((spot,i) => {
+//     spot.columns.forEach((column,j) => {
+//        let boardPositions = [];
+
+//         boardPositions.push(`r${i+1}c${j+1}|${column}`);
+//         console.log(boardPositions);
+//         return {boardPositions}
+//          })
+
+// })
+//  )
+let checkWin = (()=>{
+let r1C1 = gameBoard.board[0].columns[0];
+let r1C2 = gameBoard.board[0].columns[1];
+let r1C3 = gameBoard.board[0].columns[2];
+let r2C1 = gameBoard.board[1].columns[0];
+let r2C2 = gameBoard.board[1].columns[1];
+let r2C3 = gameBoard.board[1].columns[2];
+let r3C1 = gameBoard.board[2].columns[0];
+let r3C2 = gameBoard.board[2].columns[1];
+let r3C3 = gameBoard.board[2].columns[2];
+ 
+if(r1C1 == "X" && r1C2 == "X" && r1C3 == "X" ||
+    r1C1 == "O" && r1C2 == "O" && r1C3 == "O" ||
+    r2C1 == "X" && r2C2 == "X" && r2C3 == "X" ||
+    r2C1 == "O" && r2C2 == "O" && r2C3 == "O" ||
+    r3C1 == "X" && r3C2 == "X" && r3C3 == "X" ||
+    r3C1 == "O" && r3C2 == "O" && r3C3 == "O" ||
+    r1C1 == "X" && r2C1 == "X" && r3C1 == "X" ||
+    r1C1 == "O" && r2C1 == "O" && r2C1 == "O" ||
+    r1C2 == "X" && r2C2 == "X" && r3C2 == "X" ||
+    r1C2 == "O" && r2C2 == "O" && r2C2 == "O" ||
+    r1C3 == "X" && r2C3 == "X" && r3C3 == "X" ||
+    r1C3 == "O" && r2C3 == "O" && r2C3 == "O" ||
+    r1C1 == "X" && r2C2 == "X" && r3C3 == "X" ||
+    r3C1 == "O" && r2C2 == "O" && r1C3 == "O"
+    ){
+return true;
+}
+}
+)
+
 
 const gameController = ((playerOneName="Player One", playerTwoName="Player Two")=>{
     const players = [
@@ -69,24 +97,63 @@ const gameController = ((playerOneName="Player One", playerTwoName="Player Two")
     ]
     console.log(players);
 
-    const playRound = players.forEach((player)=>{
+    // let i=0
+    let isResult = false;
+    let player;
+    while (!isResult){
+     
+        // alert(players[i].name)
+
+     const playRound = (() => {console.log("playRound ran");
+        for(i =0; i < 2 ; i++){
+
+        if(isResult){
+        
+            break
+        }
+
+ 
+        player = players[i];
         let row;
         let column;
         row = prompt(`${player.name}, select row`);
-        console.log({row})
-        column = prompt(`${player.name}, slect column`);
-        console.log({column})
+        console.log({row});
+        column = prompt(`${player.name}, select column`);
+        console.log({column});
+        let checkBoard = (()=> { console.log("ran Checkboard");
+            if(gameBoard.board[row-1].columns[column-1]){
+                row= prompt("Move is taken, please select another row");
+                column= prompt("Please select another column");
+
+            }})();
+        checkBoard;
         gameBoard.playerMove(row,column,player.marker)
-        console.log (`${player.name} places ${player.marker} on row ${row} and column ${column}`);
+            console.log (`${player.name} places ${player.marker} on row ${row} and column ${column}`);
+
         let getBoard = console.table(gameBoard.board[0].columns+ "\n" +gameBoard.board[1].columns+" \n" +gameBoard.board[2].columns);
-    });
+        // i < 2? i = ++i:
+        // i = 0;
+        if(checkWin()){
+                isResult = true;
+                console.log(`${player.name} wins!!`);
+            }
+            // checkWin();
+            // return{
+            //     player
+            // }
+        }
+                
+        })()}
+
+
     return{
         players,
         playerOneName,
         playerTwoName,
-        playRound
     }    
-    })();
+})();
+
+
 
 
 
